d82b88f9b39cea9aa84ed5684f5054862632a355
Renan Rodrigo <renan@linuxmail.org>
2022-10-25 20:52:31 -0300

    calcular o preco total baseado nos precos unitarios

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 const botaoAdicionar = document.getElementById("botao_add");
 const lista = document.getElementById("lista");
+const total = document.getElementById("total");
 
 botaoAdicionar.onclick = function(event){
     let item = document.getElementById("input_item").value;
-    let quantidade = document.getElementById("input_qtd").value;
-    let preco = document.getElementById("input_preco").value;
+    let quantidade = Number(document.getElementById("input_qtd").value);
+    let preco = Number(document.getElementById("input_preco").value);
+
+    precoTotal = (preco * quantidade);
 
     novoItem = document.createElement("li");
-    novoItem.innerText = item + "\t" + quantidade + "\t" + preco
+    novoItem.innerText = item + "\t" + quantidade + "\t" + preco + "\t" + precoTotal;
 
     lista.appendChild(novoItem);
+
+    total.innerText = Number(total.innerText) + precoTotal;
 }
